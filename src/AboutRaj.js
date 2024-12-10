badafffdce69ac86f0f3f634c40be344061da45f
ryanhill2 <ryanhill00101@gmail.com>
2024-12-10 21:52:08 +0000

    Adding about Raj section to website

--- src/AboutRaj.js
+++ src/AboutRaj.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AboutRaj.css';
 
 const AboutRaj = () => {
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleClick = () => {
+        setShowMessage(true);
+    };
+
     return (
         <div className="about-raj">
-            <div className="message-container">
-                <p><strong>Raj Loves stealing rice from her mum & dad</strong></p>
-                <img src={`${process.env.PUBLIC_URL}/raj-rice.png`} alt="Raj Rice" />
-            </div>
+            {!showMessage && (
+                <button className="learn-more-button" onClick={handleClick}>Start learning about Raj</button>
+            )}
+            {showMessage && (
+                <div className="message-container">
+                    <p><strong>Raj Loves stealing rice from her mum & dad</strong></p>
+                    <img src={`${process.env.PUBLIC_URL}/raj-rice.png`} alt="Raj Rice" />
+                </div>
+            )}
         </div>
     );
 };
